977d3da532e60a3434d2dcdc3eb5bb5751134b59
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-01 20:38:06 +0600

    Fix vulnerability in func.php

--- func.php
+++ func.php
@@ -1,31 +1,4 @@
 <?php 
-require __DIR__.'/vendor/autoload.php';
-include("db.php");
-
-use League\CommonMark\CommonMarkConverter;
-
-$converter = new CommonMarkConverter(['html_input' => 'escape', 'allow_unsafe_links' => false]);
-
-if (isset($_GET['edid'])){
-
-    $id = $_GET['edid'];
-
-$stmt = $conn->prepare('SELECT * FROM task WHERE id = ?');
-$stmt->bind_param('i', $id);
-$stmt->execute();
-$result = $stmt->get_result();
-    $result = mysqli_query($conn, $query);
-
-    if(mysqli_num_rows($result) == 1){
-        $row = mysqli_fetch_array($result);
-        $title = $row['title'];
-
-        $_SESSION['message'] = 'Edit Task';
-        $_SESSION['message_type'] = 'info';
-    }
-}
-
-?>
     require __DIR__.'/vendor/autoload.php';
 	include("db.php");
 
@@ -33,7 +6,7 @@ $result = $stmt->get_result();
 
     $converter = new CommonMarkConverter(['html_input' => 'escape', 'allow_unsafe_links' => false]);
 
-	if (isset($_GET['edid'])){
+$id = isset($_GET['edid']) ? intval($_GET['edid']) : null;
 
 	    $id = $_GET['edid'];
 
