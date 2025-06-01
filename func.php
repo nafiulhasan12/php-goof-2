69ab37951d9ac99a3b6d0d3b5c0821855740ebb3
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-01 20:38:49 +0600

    Fix vulnerability in func.php

--- func.php
+++ func.php
@@ -11,9 +11,9 @@
 	    $id = $_GET['edid'];
 
 $stmt = $conn->prepare('SELECT * FROM task WHERE id = ?');
-$stmt = $conn->prepare('SELECT * FROM task WHERE id = ?');
+$stmt->bind_param('i', $id);
 $stmt->execute();
-$result = $stmt->get_result();
+        $result = mysqli_query($conn, $query);
 	    $result = mysqli_query($conn, $query);
 
 	    if(mysqli_num_rows($result) == 1){
