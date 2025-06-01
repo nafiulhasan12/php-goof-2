3f88197c0c14427fcf82778fc3acd8906e1cdb37
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-01 19:37:54 +0600

    Fix vulnerability in tasks.php

--- tasks.php
+++ tasks.php
@@ -8,7 +8,9 @@ if(isset($_POST['save_task'])){
 
     if(isset($_POST['edid'])) { 
         $edid = $_POST['edid'];
-        $query = "UPDATE task SET title = '$title' WHERE id = '$edid'";
+$stmt = $conn->prepare("UPDATE task SET title = ? WHERE id = ?");
+$stmt->bind_param('si', $title, $edid);
+$stmt->execute();
     }
     else $query = "INSERT INTO task(title) VALUES ('$title')";
     $result = mysqli_query($conn, $query);
