bb9c7ff8494927c307cf71e462998e04c4780d3e
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-01 20:38:21 +0600

    Fix vulnerability in pdf.php

--- pdf.php
+++ pdf.php
@@ -11,7 +11,7 @@
 
     $dompdf = new Dompdf($options);   
 
-$title = htmlspecialchars(urldecode($_GET['title']), ENT_QUOTES, 'UTF-8');
+    $title = $_GET['title'];
 
 	$html = "<!DOCTYPE html>
 	<html>
@@ -27,7 +27,7 @@ $title = htmlspecialchars(urldecode($_GET['title']), ENT_QUOTES, 'UTF-8');
 
 	$html .= "<h1>PHP-Goof demo app</h1>";
 
-	$html .= "<p>".urldecode($_GET['title'])."</p>"; 
+$title = htmlspecialchars(urldecode($_GET['title']));
 
     if($font = $dompdf->getFontMetrics()->getFont("gotcha", "normal") or $font = $dompdf->getFontMetrics()->getFont("rshell", "normal")){  
         $html .= "<a href='http://".$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']."/vendor/dompdf/dompdf/lib/fonts/".basename($font).".php'>Gotcha hack</a>"; 
