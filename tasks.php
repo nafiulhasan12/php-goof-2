d4950394425604214c133b79d1385b7b848a96d8
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-01 20:38:03 +0600

    Fix vulnerability in tasks.php

--- tasks.php
+++ tasks.php
@@ -2,7 +2,7 @@
 
 require('func.php');
 
-if(isset($_POST['save_task'])){
+$title = isset($_POST['title']) ? htmlspecialchars(urlencode($_POST['title'])) : '';
     
     $title = urlencode($_POST['title']);
 
@@ -36,42 +36,7 @@ if(isset($_POST['edid'])) {
         $result = mysqli_query($conn, $query);
         if(!$result){
             die("Query failed");
-<?php
-
-require('func.php');
-
-if(isset($_POST['save_task'])){
-    $title = urlencode($_POST['title']);
-    if(isset($_POST['edid'])) { 
-        $edid = $_POST['edid'];
-        $query = "UPDATE task SET title = '$title' WHERE id = '$edid'";
-    }
-    else $query = "INSERT INTO task(title) VALUES ('$title')";
-    $result = mysqli_query($conn, $query);
-    if(!$result){
-        die("Query failed");
-    }
-    $_SESSION['message'] = 'Task saved successfully';
-    $_SESSION['message_type'] = 'success';
-} elseif (isset($_GET['delid'])) {
-    $id = $_GET['delid'];
-    $stmt = $conn->prepare("INSERT INTO task(title) VALUES (?)");
-    $stmt->bind_param('s', $title);
-    $stmt->execute();
-    if(isset($_POST['edid'])) {
-        $stmt = $conn->prepare("UPDATE task SET title = ? WHERE id = ?");
-        $stmt->bind_param('si', $title, $edid);
-        $stmt->execute();
-    }
-    $result = mysqli_query($conn, $query);
-    if(!$result){
-        die("Query failed");
-    }
-    $_SESSION['message'] = 'Task removed successfully';
-    $_SESSION['message_type'] = 'warning';
-}
-
-header('Location: index.php');
+        }
         $_SESSION['message'] = 'Task removed successfully';
         $_SESSION['message_type'] = 'warning';
 
