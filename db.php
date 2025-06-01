3479f649787891bec8ca1e671b23f0e96cdae792
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-01 20:40:51 +0600

    Fix vulnerability in db.php

--- db.php
+++ db.php
@@ -1,8 +1,34 @@
 <?php
 $connection = 'localhost';
 $username = 'phpgoof';
+$connection = 'localhost';
+$username = 'phpgoof';
 $password = 'password';
-$database = 'phpgoof'; // Use a constant or variable instead of a string literal
+$database = 'phpgoof';
+
+class Database {
+    private $connection;
+    private $username;
+    private $password;
+    private $database;
+
+    public function __construct() {
+        $this->connection = 'localhost';
+        $this->username = 'phpgoof';
+        $this->password = 'password';
+        $this->database = 'phpgoof';
+        $this->connect();
+    }
+
+    private function connect() {
+        $conn = mysqli_connect($this->connection, $this->username, $this->password, $this->database);
+    }
+}
+
+session_start();
+
+$db = new Database();
+$database = 'phpgoof';
 
 session_start();
 
