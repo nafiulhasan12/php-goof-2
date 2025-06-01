c92a6cfe6e6a382cb6727b170612ea365f5c143d
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-01 20:38:26 +0600

    Fix vulnerability in func.php

--- func.php
+++ func.php
@@ -1,6 +1,7 @@
 <?php 
     require __DIR__.'/vendor/autoload.php';
-	include("db.php");
+    require __DIR__.'/vendor/autoload.php';
+    include("db.php");
 
     use League\CommonMark\CommonMarkConverter;
 
@@ -18,7 +19,7 @@ $result = $stmt->get_result();
 
 	    if(mysqli_num_rows($result) == 1){
 	        $row = mysqli_fetch_array($result);
-$id = isset($_GET['edid']) ? intval($_GET['edid']) : null;
+	        $title = $row['title'];
 
 	        $_SESSION['message'] = 'Edit Task';
 	        $_SESSION['message_type'] = 'info';
