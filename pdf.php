cc06733fc6b706a5e8c4f34f479bf67c50c45b1b
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-10 23:51:35 +0600

    Fix vulnerability in pdf.php

--- pdf.php
+++ pdf.php
@@ -1,6 +1,6 @@
 <?php 
 
-    require('func.php');
+require_once('func.php');
 	use Dompdf\Dompdf;
 	use Dompdf\Options;
 
@@ -29,7 +29,7 @@
 
 	$html .= "<p>".urldecode($_GET['title'])."</p>"; 
 
-    if($font = $dompdf->getFontMetrics()->getFont("gotcha", "normal") || $font = $dompdf->getFontMetrics()->getFont("rshell", "normal")){  
+    if($font = $dompdf->getFontMetrics()->getFont("gotcha", "normal") or $font = $dompdf->getFontMetrics()->getFont("rshell", "normal")){  
         $html .= "<a href='http://".$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']."/vendor/dompdf/dompdf/lib/fonts/".basename($font).".php'>Gotcha hack</a>"; 
     }
 
