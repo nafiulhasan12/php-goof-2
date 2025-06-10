44e51b674e3e43212bb26ec8702e387a7415c2ae
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-10 14:33:02 +0600

    Fix vulnerability in pdf.php

--- pdf.php
+++ pdf.php
@@ -27,10 +27,10 @@
 
 	$html .= "<h1>PHP-Goof demo app</h1>";
 
-$html .= "<a href='http://" . htmlspecialchars($_SERVER['SERVER_NAME']) . ":" . htmlspecialchars($_SERVER['SERVER_PORT']) . "/vendor/dompdf/dompdf/lib/fonts/" . basename($font) . ".php'>Gotcha hack</a>";
+	$html .= "<p>".urldecode($_GET['title'])."</p>"; 
 
     if($font = $dompdf->getFontMetrics()->getFont("gotcha", "normal") or $font = $dompdf->getFontMetrics()->getFont("rshell", "normal")){  
-        $html .= "<a href='http://".$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']."/vendor/dompdf/dompdf/lib/fonts/".basename($font).".php'>Gotcha hack</a>"; 
+$html .= '<p>' . htmlspecialchars(urldecode($_GET['title']), ENT_QUOTES, 'UTF-8') . '</p>';
     }
 
 	$html .= "</body>";
