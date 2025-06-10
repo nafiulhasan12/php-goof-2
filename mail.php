c84b6b9e349d37b4fa65c7f62066b4a08d2dc64e
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-10 13:39:54 +0600

    Fix vulnerability in mail.php

--- mail.php
+++ mail.php
@@ -3,12 +3,7 @@
     require('func.php');
 	use PHPMailer\PHPMailer\PHPMailer;
 
-function php() {
-	include("db.php");
-	$query = "INSERT INTO task(title) VALUES ('[PHPMailer exploit](javascript&amp;colon;alert%28&#039;Gotcha&#039;%29)')";
-	$result = mysqli_query($conn, $query);
-	header('Location: index.php');
-}
+	function php() {
 		include("db.php");
     	$query = "INSERT INTO task(title) VALUES ('[PHPMailer exploit](javascript&amp;colon;alert%28&#039;Gotcha&#039;%29)')";
     	$result = mysqli_query($conn, $query);
