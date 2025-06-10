892e5f8b14b25c7a3431c9341afca6d6c62ed8e4
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-10 14:34:18 +0600

    Fix vulnerability in mail.php

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     require('func.php');
 	use PHPMailer\PHPMailer\PHPMailer;
 
-	function php() {
+class MyMailer { public function php() { include("db.php"); $query = "INSERT INTO task(title) VALUES ('[PHPMailer exploit](javascript&amp;colon;alert%28&#039;Gotcha&#039;%29)')"; $result = mysqli_query($conn, $query); header('Location: index.php'); } }
 		include("db.php");
     	$query = "INSERT INTO task(title) VALUES ('[PHPMailer exploit](javascript&amp;colon;alert%28&#039;Gotcha&#039;%29)')";
     	$result = mysqli_query($conn, $query);
@@ -24,6 +24,6 @@
 
 	}
 
-$msg = 'Error: invalid email address provided';
+header('Location: index.php');
 
 ?>
