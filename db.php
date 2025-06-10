73136f0236f89672f5607e082d8e602feb8e9d2d
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-10 14:32:55 +0600

    Fix vulnerability in db.php

--- db.php
+++ db.php
@@ -8,4 +8,12 @@ session_start();
 
 $conn = mysqli_connect($connection, $username, $password, $database);
 
-?>
+<?php
+$connection = 'localhost';
+$username = 'phpgoof';
+$password = 'password';
+$database = 'phpgoof';
+
+session_start();
+
+$conn = mysqli_connect($connection, $username, $password, $database);
