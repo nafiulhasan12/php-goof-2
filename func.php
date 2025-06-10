063675d0e4f65873ae88e322d0b6e185d385a9de
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-10 19:29:03 +0600

    Fix vulnerability in func.php

--- func.php
+++ func.php
@@ -10,7 +10,34 @@
 
 	    $id = $_GET['edid'];
 
+<?php 
+    require __DIR__.'/vendor/autoload.php';
+	include("db.php");
+
+    use League\CommonMark\CommonMarkConverter;
+
+    $converter = new CommonMarkConverter(['html_input' => 'escape', 'allow_unsafe_links' => false]);
+
+	if (isset($_GET['edid'])){
+
+	    $id = $_GET['edid'];
+
 $stmt = $conn->prepare('SELECT * FROM task WHERE id = ?');
+$stmt->bind_param('i', $id);
+$stmt->execute();
+$result = $stmt->get_result();
+
+	    if(mysqli_num_rows($result) == 1){
+	        $row = $result->fetch_array();
+	        $title = $row['title'];
+
+	        $_SESSION['message'] = 'Edit Task';
+	        $_SESSION['message_type'] = 'info';
+	    }
+	}
+
+
+?>
 $stmt->bind_param('i', $id);
 $stmt->execute();
 $result = $stmt->get_result();
