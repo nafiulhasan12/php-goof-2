20387e6a7fb259830855fdbaad0fe935ed1fb2af
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-10 14:33:13 +0600

    Fix vulnerability in tasks.php

--- tasks.php
+++ tasks.php
@@ -22,14 +22,14 @@ if(isset($_POST['save_task'])){
 
 } elseif (isset($_GET['delid'])) {
 
-$title = $conn->real_escape_string(urlencode($_POST['title']));
+        $id = $_GET['delid'];
 
 $stmt = $conn->prepare("INSERT INTO task(title) VALUES (?)");
 $stmt->bind_param('s', $title);
 $stmt->execute();
 
 if(isset($_POST['edid'])) {
-    $stmt = $conn->prepare("UPDATE task SET title = ? WHERE id = ?");
+$title = mysqli_real_escape_string($conn, $_POST['title']);
     $stmt->bind_param('si', $title, $edid);
     $stmt->execute();
 }
