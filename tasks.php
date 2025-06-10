e59dd1cc4cfcee53def74c18dcf8376c9c6a2374
nafiulhasan12 <nafiulhasan125884@gmail.com>
2025-06-10 14:34:09 +0600

    Fix vulnerability in tasks.php

--- tasks.php
+++ tasks.php
@@ -22,16 +22,13 @@ if(isset($_POST['save_task'])){
 
 } elseif (isset($_GET['delid'])) {
 
-    $_SESSION['message'] = 'Task saved successfully';
-    $_SESSION['message_type'] = 'success';
-    $successMessage = 'Task saved successfully';
-    $_SESSION['message'] = $successMessage;
-    $_SESSION['message_type'] = 'success';
+        $id = $_GET['delid'];
 
 $stmt = $conn->prepare("INSERT INTO task(title) VALUES (?)");
 $stmt->bind_param('s', $title);
 $stmt->execute();
-
+        $_SESSION['message'] = 'Task removed successfully';
+        $_SESSION['message_type'] = 'warning';
 if(isset($_POST['edid'])) {
     $stmt = $conn->prepare("UPDATE task SET title = ? WHERE id = ?");
     $stmt->bind_param('si', $title, $edid);
